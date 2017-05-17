ccd22ba2c37916f90aadfbc20ae1f2aafe2e6a12
Jeff <jeffcheung2015@gmail.com>
2017-05-17 18:29:18 +0800

    stage3

---
+++ app/src/main/java/com/cse/csci3310/group10project/Stage3FindKeyActivity.java
@@ -0,0 +1,13 @@
+package com.cse.csci3310.group10project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Stage3FindKeyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stage3_find_key);
+    }
+}
